5b2fe0e7d8f09d69e27d226b0a56fa02158f0de5
Влад Начатой <subpolare@MacBook-Air.local>
2025-07-05 22:11:05 +0300

    Add first calendar

--- life-calendar.py
+++ life-calendar.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import bisect
 import numpy as np
+from PIL import Image
 from typing import NamedTuple
 from datetime import date, timedelta
 
@@ -19,15 +20,13 @@ plt.rcParams['font.weight'] = 'regular'
 
 TITLE_FONT =    {'family': 'Montserrat', 'weight': 'black',   'size': plt.rcParams.get('font.size', 12) * 4.4}
 SUBTITLE_FONT = {'family': 'Montserrat', 'weight': 'black',   'size': plt.rcParams.get('font.size', 12) * 1.115}
-BLACK_FONT =    {'family': 'Montserrat', 'weight': 'bold',    'size': plt.rcParams.get('font.size', 12) * 0.6}
+BLACK_FONT =    {'family': 'Montserrat', 'weight': 'black',   'size': plt.rcParams.get('font.size', 12) * 0.6}
+BOLD_FONT =     {'family': 'Montserrat', 'weight': 'bold',    'size': plt.rcParams.get('font.size', 12) * 0.6}
 TEXT_FONT =     {'family': 'Montserrat', 'weight': 'regular', 'size': plt.rcParams.get('font.size', 12) * 0.6}
 
 # ———————————————————————————————————————— CALENDAR RENDERING ————————————————————————————————————————
 
 def create_calendar(birthday, life: list[LifeStage], fname: str = 'calendar-of-life.png', h = 13, w = 9):
-    
-    # Initial settings 
-    
     plt.rcParams.update(plt.rcParamsDefault)
     fig, ax = plt.subplots(figsize = (w, h))
     ax.set_axis_off()
@@ -44,7 +43,7 @@ def create_calendar(birthday, life: list[LifeStage], fname: str = 'calendar-of-l
     facecolors = {e.stage : e.color for e in life[1:]}
     edgecolors = {e.stage : e.color for e in life[1:]} 
     weeks = np.linspace(0, h, 52)
-    years = np.linspace(w, 0, 80)
+    years = np.linspace(w, 0, 81)
 
     for year in years:
         for week in weeks:
@@ -59,18 +58,18 @@ def create_calendar(birthday, life: list[LifeStage], fname: str = 'calendar-of-l
 
     # Adding design elements
 
-    plt.text(6.5, 9.85, 'Календарь жизни', ha = 'center', **TITLE_FONT) 
-    plt.text(6.5, 9.55, 'Время на Земле ограничено и очень важно. На что ты его потратишь?', ha = 'center', **SUBTITLE_FONT)
+    plt.text(6.3, 10, 'Календарь жизни', ha = 'center', va = 'center', **TITLE_FONT) 
+    plt.text(6.3, 9.6, 'Время на Земле ограничено и очень важно. На что ты его потратишь?', ha = 'center', va = 'center', **SUBTITLE_FONT)
 
     # The right arrow 
-    arrow = FancyArrowPatch(
-        posA = (13.8, 9.07), posB = (13.8, 7.9),
-        arrowstyle = '-|>', connectionstyle = 'arc3, rad = 0',
-        mutation_scale = 10, linewidth = 1,
-        color = 'k', capstyle = 'round'
-    )
-    plt.gca().add_patch(arrow)
-    plt.text(13.6, 8.165, 'ЖИЗНЕННЫЙ ЭТАП', ha = 'center', **BLACK_FONT, rotation = 270) 
+    # arrow = FancyArrowPatch(
+    #     posA = (14, 9.07), posB = (14, 7.9),
+    #     arrowstyle = '-|>', connectionstyle = 'arc3, rad = 0',
+    #     mutation_scale = 10, linewidth = 1,
+    #     color = 'k', capstyle = 'round'
+    # )
+    # plt.gca().add_patch(arrow)
+    # plt.text(13.8, 8.165, 'ЖИЗНЕННЫЙ ЭТАП', ha = 'center', **BLACK_FONT, rotation = 270) 
 
     # The left arrow 
     arrow = FancyArrowPatch(
@@ -80,7 +79,7 @@ def create_calendar(birthday, life: list[LifeStage], fname: str = 'calendar-of-l
         color = 'k', capstyle = 'round'
     )
     plt.gca().add_patch(arrow)
-    plt.text(-0.8, 8.63, 'ВОЗРАСТ', ha = 'center', **BLACK_FONT, rotation = 270) 
+    plt.text(-0.8, 8.63, 'ВОЗРАСТ', ha = 'center', **BOLD_FONT, rotation = 270) 
 
     # Upper arrow
     arrow = FancyArrowPatch(
@@ -90,15 +89,27 @@ def create_calendar(birthday, life: list[LifeStage], fname: str = 'calendar-of-l
         color = 'k', capstyle = 'round'
     )
     plt.gca().add_patch(arrow)
+    plt.text(0.8, 9.23, 'НЕДЕЛИ В ГОДУ', ha = 'center', **BOLD_FONT) 
+
+    # Stages of life
+    plt.text(13.4, 8.53, 'РАННЕЕ\nДЕТСТВО', ha = 'center', **BLACK_FONT, rotation = 270, color = 'C0') 
+    plt.text(13.4, 7.28, 'ШКОЛЬНЫЕ ГОДЫ',   ha = 'center', **BLACK_FONT, rotation = 270, color = 'C1') 
+    plt.text(13.4, 6.38, 'УНИВЕРСИТЕТ',     ha = 'center', **BLACK_FONT, rotation = 270, color = 'C2') 
+    plt.text(13.4, 3.60, 'ВЗРОСЛАЯ ЖИЗНЬ',  ha = 'center', **BLACK_FONT, rotation = 270, color = 'C3')
+    plt.text(13.4, 0.70, 'СТАРОСТЬ',        ha = 'center', **BLACK_FONT, rotation = 270, color = 'C4') 
 
     # Numbering of years
     for i, year in enumerate(years):
         if (i) % 10 == 0 or i == 0:
-            ax.text(-0.4, year, f'{i}', horizontalalignment='center', verticalalignment='center', **BLACK_FONT)
+            ax.text(-0.4, year, f'{i}', horizontalalignment='center', verticalalignment='center', **BOLD_FONT)
         else: 
             ax.text(-0.4, year, f'{i}', horizontalalignment='center', verticalalignment='center', **TEXT_FONT)
 
-    plt.savefig(fname, dpi = 300)
+    # Image postprocessing
+    plt.savefig(fname, dpi = 600)
+    img = Image.open(fname)
+    cropped = img.crop((700, 350, img.width - 440, img.height - 1000))
+    cropped.save(fname, dpi = (600, 600))
 
 # ———————————————————————————————————————— INITIALIZING ————————————————————————————————————————
 
@@ -112,9 +123,9 @@ if __name__ == '__main__':
     life = [
         LifeStage('Рождение', birthday),
         LifeStage('Раннее детство', birthday + timedelta(days = 6 * 365.25 + 7),  'C0'),
-        LifeStage('Школьные годы', birthday + timedelta(days = 17 * 365.25 + 7), 'C1'),
-        LifeStage('Университет', birthday + timedelta(days = 24 * 365.25), 'C2'),
-        LifeStage('Взрослая жизнь', birthday + timedelta(days = 64 * 365.25), 'C4'),
-        LifeStage('Старость', birthday + timedelta(days = 99 * 365.25), 'C5'),
+        LifeStage('Школьные годы', birthday + timedelta(days = 18 * 365.25 + 7), 'C1'),
+        LifeStage('Университет', birthday + timedelta(days = 24 * 365.25 + 7), 'C2'),
+        LifeStage('Взрослая жизнь', birthday + timedelta(days = 64 * 365.25), 'C3'),
+        LifeStage('Старость', birthday + timedelta(days = 99 * 365.25), 'C4'),
     ]
     create_calendar(birthday, life, fname = 'calendar-of-life.png')
