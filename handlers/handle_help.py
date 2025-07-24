6fa36731a1f3001a6dde5c63416453d4ab912d8f
Влад Начатой <subpolare@MacBook-Air.local>
2025-07-24 16:59:05 +0300

    Update /calendar

--- handlers/handle_help.py
+++ handlers/handle_help.py
@@ -1,23 +1,24 @@
 from telegram.ext import ContextTypes
-from telegram import Update
-
-import asyncio, warnings
-from dotenv import load_dotenv
-
 from utils.typing import _keep_typing
+from dotenv import load_dotenv
+from telegram import Update
+import asyncio, warnings
 
 warnings.filterwarnings('ignore')
 load_dotenv()
 
 async def handle_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Send help instructions to the user."""
     stop_event  = asyncio.Event()
-    typing_task = context.application.create_task(
-        _keep_typing(update.effective_chat.id, context.bot, stop_event)
-    )
+    typing_task = context.application.create_task(_keep_typing(update.effective_chat.id, context.bot, stop_event))
 
     help_text = (
-        'TODO: describe how to use the bot here.'
+        'Вот небольшой список того, что я умею\n\n'
+        '/start — Начать наше знакомство с самого начала\n\n'
+        '/calendar — Нарисовать календарь с любым событием из твоей жизни\n\n'
+        '/me — Узнать, что я знаю о тебе. Здесь же можно отредактировать эти данные\n\n'
+        '/oblivion — Безвозвратно удалить все данные о себе. После нажатия на эту команду я еще раз спрошу тебя, уверены ли ты в этом, так что можешь не бояться нажать на эту кнопку случайно\n\n'
+        '/cancel — Если я вдруг зависну, эта команда может помочь меня перезапустить (а может и не помочь...)\n\n'
+
     )
 
     stop_event.set()
